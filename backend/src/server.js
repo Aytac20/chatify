13a6898038e8179624f56ea30c8950de4c6bdb68
Aytac <abalayeva12@gmail.com>
2026-02-04 17:25:46 +0400

    change in package.json file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
