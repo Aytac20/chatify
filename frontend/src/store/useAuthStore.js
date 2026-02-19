ca719c3e6f1567cb877d43af5e9a7e791aa5f603
Aytac <abalayeva12@gmail.com>
2026-02-19 14:20:52 +0400

    realtime messages

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,33 +2,38 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
+
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
-  isImageUpdating: false,
+  isLoggingIn: false,
   socket: null,
   onlineUsers: [],
+
   checkAuth: async () => {
     try {
-      const res = await axiosInstance("/auth/check");
+      const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
-      console.log("Error in authCheck", error);
+      console.log("Error in authCheck:", error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
     }
   },
+
   signup: async (data) => {
     set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
-      toast.success("Account created successfuly");
+
+      toast.success("Account created successfully!");
       get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
@@ -36,12 +41,15 @@ export const useAuthStore = create((set, get) => ({
       set({ isSigningUp: false });
     }
   },
+
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
-      toast.success("Logged in  successfuly");
+
+      toast.success("Logged in successfully");
+
       get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
@@ -49,46 +57,48 @@ export const useAuthStore = create((set, get) => ({
       set({ isLoggingIn: false });
     }
   },
+
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
-      toast.success("Logged out successfuly");
+      toast.success("Logged out successfully");
       get().disconnectSocket();
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
     }
   },
+
   updateProfile: async (data) => {
-    set({ isImageUpdating: true });
     try {
       const res = await axiosInstance.put("/auth/update-profile", data);
       set({ authUser: res.data });
-      toast.success("Profile updated successfuly");
+      toast.success("Profile updated successfully");
     } catch (error) {
-      console.log("Error in update profile", error);
+      console.log("Error in update profile:", error);
       toast.error(error.response.data.message);
-    } finally {
-      set({ isImageUpdating: false });
     }
   },
+
   connectSocket: () => {
     const { authUser } = get();
-
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      withCredentials: true,
+      withCredentials: true, // this ensures cookies are sent with the connection
     });
 
     socket.connect();
 
     set({ socket });
 
+    // listen for online users event
     socket.on("getOnlineUsers", (userIds) => {
       set({ onlineUsers: userIds });
     });
   },
+
   disconnectSocket: () => {
     if (get().socket?.connected) get().socket.disconnect();
   },
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { useAuthStore } from "./useAuthStore";
+
 export const useChatStore = create((set, get) => ({
   allContacts: [],
   chats: [],
@@ -11,12 +12,15 @@ export const useChatStore = create((set, get) => ({
   isUsersLoading: false,
   isMessagesLoading: false,
   isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+
   toggleSound: () => {
     localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
     set({ isSoundEnabled: !get().isSoundEnabled });
   },
+
   setActiveTab: (tab) => set({ activeTab: tab }),
   setSelectedUser: (selectedUser) => set({ selectedUser }),
+
   getAllContacts: async () => {
     set({ isUsersLoading: true });
     try {
@@ -39,6 +43,7 @@ export const useChatStore = create((set, get) => ({
       set({ isUsersLoading: false });
     }
   },
+
   getMessagesByUserId: async (userId) => {
     set({ isMessagesLoading: true });
     try {
@@ -50,9 +55,11 @@ export const useChatStore = create((set, get) => ({
       set({ isMessagesLoading: false });
     }
   },
+
   sendMessage: async (messageData) => {
     const { selectedUser, messages } = get();
     const { authUser } = useAuthStore.getState();
+
     const tempId = `temp-${Date.now()}`;
 
     const optimisticMessage = {
@@ -62,9 +69,11 @@ export const useChatStore = create((set, get) => ({
       text: messageData.text,
       image: messageData.image,
       createdAt: new Date().toISOString(),
-      isOptimictic: true,
+      isOptimistic: true, // flag to identify optimistic messages (optional)
     };
+    // immidetaly update the ui by adding the message
     set({ messages: [...messages, optimisticMessage] });
+
     try {
       const res = await axiosInstance.post(
         `/messages/send/${selectedUser._id}`,
@@ -72,8 +81,38 @@ export const useChatStore = create((set, get) => ({
       );
       set({ messages: messages.concat(res.data) });
     } catch (error) {
+      // remove optimistic message on failure
       set({ messages: messages });
       toast.error(error.response?.data?.message || "Something went wrong");
     }
+  },
+
+  subscribeToMessages: () => {
+    const { selectedUser, isSoundEnabled } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      const currentMessages = get().messages;
+      set({ messages: [...currentMessages, newMessage] });
+
+      if (isSoundEnabled) {
+        const notificationSound = new Audio("/sounds/notification.mp3");
+        notificationSound.currentTime = 0; // reset to start
+        notificationSound
+          .play()
+          .catch((e) => console.log("Audio play failed:", e));
+      }
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
   },
 }));
